e89e7c867f8a095026e079f78c110a8d2f8dc83c
Yuliia Horenko <yuliia_horenko@epam.com>
2019-09-09 15:59:54 +0300

    in working

--- src/main/java/com/tasks/Date.java
+++ src/main/java/com/tasks/Date.java
@@ -6,13 +6,19 @@ import org.joda.time.LocalDateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.Calendar;
+import java.util.Locale;
+
+import static org.joda.time.DateTimeFieldType.dayOfWeek;
+
 public class Date{
+
     public static void main(String[] args) {
 
         LocalDate currentDate = LocalDate.now();
         System.out.println(currentDate);
-        LocalDateTime currentTime = LocalDateTime.now();
-
 
         String date = String.valueOf(currentDate);
         DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd");
@@ -23,14 +29,21 @@ public class Date{
         System.out.println(dateformatOut.print(jodaTime));
 
         System.out.println(jodaTime.withTimeAtStartOfDay().getMillis());
-        System.out.println(jodaTime.millisOfDay().withMaximumValue().getMillis());
+        long longDate = jodaTime.millisOfDay().withMaximumValue().getMillis();
+        System.out.println(longDate);
 
+        DateTimeFormatter convertLongToStr = DateTimeFormat.forPattern("yyyy-MM-dd.HH.mm.ss.SS");
+        System.out.println(convertLongToStr.print(longDate));
 
-
-
-
-
+//       String dayOfWeek = String.valueOf(dayOfWeek(longDate));
+//        System.out.println(dayOfWeek);
+//
+//
+//    }
+//    private static Object dayOfWeek(long longDate) {
+//        return String.valueOf(dayOfWeek(longDate));
     }
+
 }
 
 
